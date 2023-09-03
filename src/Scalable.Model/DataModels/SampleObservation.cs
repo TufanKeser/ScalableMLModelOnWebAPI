4a11ab0d64bca2eaaaee824caa517c7f800d124c
Torna <Torna@TO_FUN>
2023-09-03 18:13:47 +0300

    Add project files.

---
+++ src/Scalable.Model/DataModels/SampleObservation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+
+namespace Scalable.Model.DataModels
+{
+    public class SampleObservation
+    {
+        [ColumnName("Label")]
+        public bool IsToxic { get; set; }
+
+
+        [ColumnName("Text")]
+        public string SentimentText { get; set; }
+
+    }
+}
